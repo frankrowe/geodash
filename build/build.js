8a39e7e9d6573e4354fa9c5a0d0082ca18dd69f7
Frank Rowe <fsrowe@gmail.com>
2013-06-18 17:50:01 -0400

    added styles + initial chart inheritance

--- build/build.js
+++ build/build.js
@@ -1,8 +1,18 @@
 var fs = require('fs'),
-    UglifyJS = require('uglify-js');
+    UglifyJS = require('uglify-js'),
+    lesscss = require('less');
 
-var result = UglifyJS.minify(["../src/GeoDash.js", "../src/Chart/Chart.js"]);
+var result = UglifyJS.minify(["../src/GeoDash.js", "../src/Chart/Chart.js", "../src/Chart/BarChart.js"]);
 fs.writeFile('../dist/GeoDash.min.js', result.code, function (err) {
   if (err) throw err;
   console.log('It\'s saved!');
 });
+
+fs.readFile('../src/Styles/chart.less', 'utf8', function(err, less){
+  lesscss.render(less, function (e, css) {
+    fs.writeFile('../dist/GeoDash.min.css', css, function (err) {
+      if (err) throw err;
+      console.log('It\'s saved!');
+    });
+  });
+})
