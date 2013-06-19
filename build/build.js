6dc86047e94b1491e86fee148caf3ed3c75d767f
Frank Rowe <fsrowe@gmail.com>
2013-06-19 15:11:24 -0400

    added source map

--- build/build.js
+++ build/build.js
@@ -2,8 +2,16 @@ var fs = require('fs'),
     UglifyJS = require('uglify-js'),
     lesscss = require('less');
 
-var result = UglifyJS.minify(["../src/GeoDash.js", "../src/Chart/Chart.js", "../src/Chart/BarChart.js"]);
+var result = UglifyJS.minify(["../src/GeoDash.js", "../src/Chart/Chart.js", "../src/Chart/BarChart.js", "../src/Chart/LineChart.js"], {
+  outSourceMap: "GeoDash.js.map",
+  output: { beautify: true}
+});
 fs.writeFile('../dist/GeoDash.min.js', result.code, function (err) {
+  if (err) throw err;
+  console.log('It\'s saved!');
+});
+
+fs.writeFile('../dist/GeoDash.js.map', result.map, function (err) {
   if (err) throw err;
   console.log('It\'s saved!');
 });
