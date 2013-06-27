e78210c3e690ad7b2ef48c09a81ac2b512699d5d
Frank Rowe <fsrowe@gmail.com>
2013-06-27 14:41:28 -0400

    implemented pie updating

--- src/Chart/PieChart.js
+++ src/Chart/PieChart.js
@@ -70,18 +70,6 @@ GeoDash.PieChart = ezoop.ExtendedClass(GeoDash.Chart, {
       .attr("class", "piechart-svg")
       .append("g")
         .attr("transform", "translate(" + this.width / 2 + "," + this.height / 2 + ")");
-  },
-  update: function(data){
-    var self = this;
-    data.forEach(function(d) {
-      d[self.options.value] = +d[self.options.value];
-    });
-
-    this.x.domain([0, 1]);
-    this.y.domain([0, 1]);
-
-    this.xAxis.tickValues([0, 1]);
-    this.yAxis.tickValues([0, 1]);
 
     if(this.options.drawX){
       this.svg.append("g")
@@ -102,41 +90,72 @@ GeoDash.PieChart = ezoop.ExtendedClass(GeoDash.Chart, {
         .attr("transform", "translate(5,0)");
     }
 
-    var g = this.svg.selectAll(".arc")
-        .data(this.pie(data))
-        .enter().append("g")
-          .attr("class", "arc");
+    if(this.options.legend) {
+      d3.select(this.options.legend).append('svg');
+    }
+  },
+  update: function(data){
+    var self = this;
+    data.forEach(function(d) {
+      d[self.options.value] = +d[self.options.value];
+    });
 
-    g.append("path")
-      .attr("d", this.arc)
-      .style("fill", function(d) { return self.color(d.data[self.options.label]); })
-      .style("fill-opacity", this.options.opacity)
-      .on('mouseover', function(d,i){
-        d3.select(self.el).select('.hoverbox').html(d.value + '%');
-        d3.select(this).style('fill-opacity', 1);
-      })
-      .on('mouseout', function(d,i){
-        d3.select(self.el).select('.hoverbox').html('');
-        d3.select(this).style('fill-opacity', self.options.opacity  );
-      });
+    this.x.domain([0, 1]);
+    this.y.domain([0, 1]);
+
+    this.xAxis.tickValues([0, 1]);
+    this.yAxis.tickValues([0, 1]);
+
+    if(this.options.drawX){
+      svg.select(".x.axis").transition()
+        .call(this.xAxis);
+    }
+
+    if(this.options.drawY){
+      svg.select(".y.axis").transition()
+        .call(this.yAxis);
+    }
+
+    var g = this.svg.selectAll(".arc")
+        .data(this.pie(data));
+
+    g.select("path").transition().attr("d", this.arc);
+
+    g.enter().append("g")
+      .attr("class", "arc")
+        .append("path")
+        .attr("d", this.arc)
+        .style("fill", function(d) { return self.color(d.data[self.options.label]); })
+        .style("fill-opacity", this.options.opacity)
+        .on('mouseover', function(d,i){
+          d3.select(self.el).select('.hoverbox').html(d.value + '%');
+          d3.select(this).style('fill-opacity', 1);
+        })
+        .on('mouseout', function(d,i){
+          d3.select(self.el).select('.hoverbox').html('');
+          d3.select(this).style('fill-opacity', self.options.opacity  );
+        });
 
     if(this.options.legend) {
       var lWidth = parseInt(d3.select(this.options.legend).style('width'));
       var block = {width: 10, height: 10, padding: 5};
-      var container = d3.select(this.options.legend).append('svg');
+      var container = d3.select(this.options.legend).select('svg');
       var legend = container.selectAll(".legend-item")
-          .data(this.color.domain().slice().reverse())
-        .enter().append("g")
+          .data(this.color.domain().slice().reverse());
+
+      legend.select(".legend-item").select("text").text(function(d) { return d; });
+
+      var legenditem = legend.enter().append("g")
           .attr("class", "legend-item")
           .attr("transform", function(d, i) { return "translate(0," + i * (block.height + block.padding) + ")"; });
 
-      legend.append("rect")
+      legenditem.append("rect")
         .attr("x", 0)
         .attr("width", block.width)
         .attr("height", block.height)
         .style("fill", this.color);
 
-      legend.append("text")
+      legenditem.append("text")
         .attr("x", (block.width + block.padding))
         .attr("y", 4)
         .attr("dy", ".35em")
